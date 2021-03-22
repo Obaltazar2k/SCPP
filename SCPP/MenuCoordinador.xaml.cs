701d5c24e25b28d739d1c323106d2d5c18b7a406
Omar Baltazar <obaltazar2000@gmail.com>
2021-03-21 20:14:36 -0600

    Login funcional con navegación entre ventanas y bloqueo de intentos

---
+++ SCPP/MenuCoordinador.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace SCPP
+{
+    /// <summary>
+    /// Lógica de interacción para MenuCoordinador.xaml
+    /// </summary>
+    public partial class MenuCoordinador : Page
+    {
+        public MenuCoordinador()
+        {
+            InitializeComponent();
+        }
+
+        private void AssignProjectButton_Click(object sender, RoutedEventArgs e)
+        {
+            var mainWindow = (MainWindow)Application.Current.MainWindow;
+            mainWindow?.ChangeView(new AsignarProyectoEstudiante());
+            return;
+        }
+    }   
+}
