2d42c62b34d421c4708483abb5c27a3f261f7d05
Omar Baltazar <obaltazar2000@gmail.com>
2021-03-22 23:27:51 -0600

    Probando la función desasignar

--- SCPP/DesasignarProfesorGrupo.xaml.cs
+++ SCPP/DesasignarProfesorGrupo.xaml.cs
@@ -1,17 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using SCPP.Utilities;
+using WPFCustomMessageBox;
 
 namespace SCPP
 {
@@ -20,13 +12,43 @@ namespace SCPP
     /// </summary>
     public partial class DesasignarProfesorGrupo : Page
     {
+        private Profesor profesorSelected = null;
+        private Grupo groupSelected = null;
+        private string _period;
+
         public DesasignarProfesorGrupo()
         {
             InitializeComponent();
+            GetProfesors();
+            _period = Period.GetPeriod();
         }
 
         private void DesassignButton_Click(object sender, System.Windows.RoutedEventArgs e)
-        {            
+        {
+            if (profesorSelected == null || groupSelected == null)
+                return;
+
+            MessageBoxResult confirmation = CustomMessageBox.ShowYesNo("¿Seguro que desea desasignar al PROFESOR "
+                + profesorSelected.Nombre + " " + profesorSelected.Apellidopaterno + " " + profesorSelected.Apellidopaterno + " con Rfc " + profesorSelected.Rfc
+                + " del GRUPO " + groupSelected.Nrc + "?", "Confirmación", "Si", "No");
+            var assignDone = false;
+            if (confirmation == MessageBoxResult.Yes)
+            {
+                using (SCPPContext context = new SCPPContext())
+                {
+                    context.Grupo.Remove(groupSelected);
+                    context.SaveChanges();
+                    assignDone = true;
+                }
+            }
+
+            if (assignDone == true)
+            {
+                MessageBoxResult result = CustomMessageBox.Show("La desasignación ha sido realizada con éxito.");
+                var mainWindow = (MainWindow)Application.Current.MainWindow;
+                mainWindow?.ChangeView(new MenuCoordinador());
+                return;
+            }
         }
 
         private void CancelButton_Click(object sender, RoutedEventArgs e)
@@ -38,10 +60,10 @@ namespace SCPP
 
         private void GetProfesors()
         {
-            /*List<Profesor> profesorsCollection = new List<Profesor>();
+            List<Profesor> profesorsCollection = new List<Profesor>();
             using (SCPPContext context = new SCPPContext())
             {
-                //var profesorsList = context.Profesor.Where(p => );
+                var profesorsList = context.Profesor.Where(p => p.Grupo.Count >= 1);
                 if (profesorsList != null)
                 {
                     foreach (Profesor profesor in profesorsList)
@@ -51,7 +73,47 @@ namespace SCPP
                     }
                 }
             }
-            ProfesorsList.ItemsSource = profesorsCollection;*/
+            ProfesorsList.ItemsSource = profesorsCollection;
+        }
+
+        private void GetGroups()
+        {
+            List<Grupo> groupsCollection = new List<Grupo>();
+            using (SCPPContext context = new SCPPContext())
+            {
+                var groupsList = context.Grupo.Where(p => p.Rfcprofesor.Equals(profesorSelected.Rfc) && p.Periodo.Equals(_period));
+                if (groupsList != null)
+                {
+                    foreach (Grupo grupo in groupsList)
+                    {
+                        if (grupo != null)
+                            groupsCollection.Add(grupo);
+                    }
+                }
+            }
+            GroupsList.ItemsSource = groupsCollection;
+        }
+
+        private void CheckSelecctions()
+        {
+            if ((profesorSelected != null) && (groupSelected != null))
+                DesassignButton.IsEnabled = true;
+        }
+
+        private void ProfesorsList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            DataGrid dataGrid = sender as DataGrid;
+            profesorSelected = (Profesor)dataGrid.SelectedItems[0];
+            CheckSelecctions();
+
+            GetGroups();
+        }
+
+        private void GroupsList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            DataGrid dataGrid = sender as DataGrid;
+            groupSelected = (Grupo)dataGrid.SelectedItems[0];
+            CheckSelecctions();
         }
     }
 }
--- SCPP/MenuCoordinador.xaml.cs
+++ SCPP/MenuCoordinador.xaml.cs
@@ -27,6 +27,13 @@ namespace SCPP
             return;
         }
 
+        private void DesassignGroupButton_Click(object sender, RoutedEventArgs e)
+        {
+            var mainWindow = (MainWindow)Application.Current.MainWindow;
+            mainWindow?.ChangeView(new DesasignarProfesorGrupo());
+            return;
+        }
+
         private void RegisterStudentButton_Click(object sender, RoutedEventArgs e)
         {
             var mainWindow = (MainWindow)Application.Current.MainWindow;
