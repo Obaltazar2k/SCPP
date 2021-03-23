8351216de2c4de1921deeeef08ecb589607afbbf
Omar Baltazar <obaltazar2000@gmail.com>
2021-03-22 18:06:54 -0600

    Creación de UI para Desasignar profesor a grupo

---
+++ SCPP/DesasignarProfesorGrupo.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SCPP
+{
+    /// <summary>
+    /// Lógica de interacción para Desasignar_profesor_a_grupo.xaml
+    /// </summary>
+    public partial class DesasignarProfesorGrupo : Page
+    {
+        public DesasignarProfesorGrupo()
+        {
+            InitializeComponent();
+        }
+
+        private void DesassignButton_Click(object sender, System.Windows.RoutedEventArgs e)
+        {            
+        }
+
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            var mainWindow = (MainWindow)Application.Current.MainWindow;
+            mainWindow?.ChangeView(new MenuCoordinador());
+            return;
+        }
+    }
+}
