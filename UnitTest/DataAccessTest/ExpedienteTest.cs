a05b5a890ad40a26de193ca031112523265c9782
danielpale <62296781+danielpale@users.noreply.github.com>
2021-04-06 19:29:39 -0500

    Se agrego prueba unitaria de expediente
    Aún no se tienen los métodos.

---
+++ UnitTest/DataAccessTest/ExpedienteTest.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTest.DataAccessTest
+{
+    [TestClass]
+    public class ExpedienteTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
